2b3ab29097256b62c9c6f41ab5179feb8b5cbd06
Maksym Kultyshev <mksbnt@icloud.com>
2023-12-02 14:43:28 +0200

    feat: add robots.txt, delete header component, upd button

--- src/app/core/shared/ui/layout/layout.component.ts
+++ src/app/core/shared/ui/layout/layout.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   template: `
     <main><router-outlet></router-outlet></main>
     <app-footer></app-footer>
