17d0a9fc7961931f60bed3b602b3d0762e56f858
Maksym Kultyshev <mksbnt@icloud.com>
2023-12-02 11:28:11 +0200

    feat: use compression for server; specify npm version; add link;

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import compression from 'compression';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -47,7 +48,7 @@ function run(): void {
   const port = process.env['PORT'] || 4000;
 
   // Start up the Node server
-  const server = app();
+  const server = app().use(compression);
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
