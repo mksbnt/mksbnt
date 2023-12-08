a0c49e76b2ebf51d435fe787b1d9b824ef30218a
Maksym Kultyshev <mksbnt@icloud.com>
2023-12-08 21:58:00 +0200

    feat: add cashing

--- src/app/core/pipes/pixels-postfix.pipe.ts
+++ src/app/core/pipes/pixels-postfix.pipe.ts
@@ -1,13 +1,18 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'pixelsPostfix',
-  standalone: true
+  name: "pixelsPostfix",
+  standalone: true,
 })
 export class PixelsPostfixPipe implements PipeTransform {
+  private cachedValue: string | null = null;
+  private lastInput: number = -1;
 
   transform(input: number): string {
-    return `${input}px`;
+    if (this.cachedValue === null || input !== this.lastInput) {
+      this.lastInput = input;
+      this.cachedValue = `${input}px`;
+    }
+    return this.cachedValue;
   }
-
 }
--- src/app/core/pipes/seconds-postfix.pipe.ts
+++ src/app/core/pipes/seconds-postfix.pipe.ts
@@ -1,13 +1,18 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'secondsPostfix',
-  standalone: true
+  name: "secondsPostfix",
+  standalone: true,
 })
 export class SecondsPostfixPipe implements PipeTransform {
+  private cachedValue: string | null = null;
+  private lastInput: number = -1;
 
   transform(input: number): string {
-    return `${input}s`;
+    if (this.cachedValue === null || input !== this.lastInput) {
+      this.lastInput = input;
+      this.cachedValue = `${input}s`;
+    }
+    return this.cachedValue;
   }
-
 }
